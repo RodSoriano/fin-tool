37a57086d4e2401a559fb2e8de020ff16923984c
Rodrigo Soriano <rsoriano.jose@gmail.com>
2024-05-13 14:34:31 -0600

    Create pages configuration file

---
+++ app/frontend/pages.js
@@ -0,0 +1,19 @@
+import Layout from './Layouts/default.vue';
+
+const pages = import.meta.env.SSR
+  ? import.meta.globEagerDefault('./Pages/**/*.vue', { eager: true })
+  : import.meta.glob('./Pages/**/*.vue', { eager: true });
+
+export const resolvePage = async (name) => {
+  const page = pages[`./Pages/${name}.vue`];
+
+  if (!page) {
+    throw new Error(
+      `Unknown page ${name}. Is it located under Pages with a .vue extension?`
+    );
+  }
+
+  page.default.layout = page.default.layout || Layout;
+
+  return import.meta.env.SSR ? page : (await page).default;
+};
