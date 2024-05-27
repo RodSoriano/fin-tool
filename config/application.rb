d5c5d3358f3590027458d8c7722565bd84bffcf6
Rodrigo Soriano <rsoriano.jose@gmail.com>
2024-05-27 15:31:30 -0600

    Update paths with eager loaded folders in app/models

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,6 @@ module FinTool
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load_paths += %W(#{config.root}/app/models)
   end
 end
