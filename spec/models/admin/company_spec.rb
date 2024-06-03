72800765579c0f8fa7a78a3f4369f9a4de9959ae
Rodrigo Soriano <rsoriano.jose@gmail.com>
2024-06-03 01:53:02 -0600

    Update company specs with associations block

--- spec/models/admin/company_spec.rb
+++ spec/models/admin/company_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Admin::Company, type: :model do
+  context 'associations' do
+    it { should have_many(:company_users) }
+    it { should have_many(:users).through(:company_users) }
+  end
+
   context 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
