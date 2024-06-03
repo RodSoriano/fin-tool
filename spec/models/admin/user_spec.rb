da30ca824f44238d25bddca33b4e71396a772c81
Rodrigo Soriano <rsoriano.jose@gmail.com>
2024-06-03 01:52:52 -0600

    Update user specs with associations block

--- spec/models/admin/user_spec.rb
+++ spec/models/admin/user_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Admin::User, type: :model do
+  context 'associations' do
+    it { should have_many(:company_users) }
+    it { should have_many(:companies).through(:company_users) }
+  end
+
   context 'validations' do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
