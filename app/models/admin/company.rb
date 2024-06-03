5a98f6a10ac143f0c83221df4d9ba9b26c5ee9e9
Rodrigo Soriano <rsoriano.jose@gmail.com>
2024-06-03 01:25:33 -0600

    Update with has_many association in Company model

--- app/models/admin/company.rb
+++ app/models/admin/company.rb
@@ -1,4 +1,10 @@
 class Admin::Company < ApplicationRecord
+	has_many :company_users,
+		class_name: 'Admin::CompanyUser',
+		inverse_of: 'company'
+
+	has_many :users, through: :company_users
+
 	validates :name, presence: true, uniqueness: true
 	validates :email, presence: true, uniqueness: true
 	validates :location, presence: true
